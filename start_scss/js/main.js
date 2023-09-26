098ca69dcd1822fa2284ddf6344dd752e644ed3c
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2023-09-26 11:35:35 +0200

    exo1js

---
+++ start_scss/js/main.js
@@ -0,0 +1,12 @@
+let nom = "SAÏD";
+let prenom = "Meldryck";
+
+console.log(nom);
+console.log(prenom);
+
+console.log(prenom + " " + nom);
+// ou
+console.log(`je m'appelle : ${prenom}  ${nom} `);
+
+//numbers
+
