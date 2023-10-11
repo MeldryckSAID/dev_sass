13c4a7b6362b09169ab30817e1678f6028d330ed
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2023-10-11 11:21:15 +0200

    instalation nuxt

--- src/components/elements/MyCards.vue
+++ src/components/elements/MyCards.vue
@@ -1,54 +1,36 @@
 <script setup>
-import MyCard from './MyCard.vue';
-import { reactive } from 'vue';
+import MyCard from './MyCard.vue'
+import { reactive } from 'vue'
 
 const response = [
   {
+    id: '1',
     description: 'Description de la card',
     title: '100% Gratuit',
     imageSrc: 'http://placekitten.com/300/150',
     imageAlt: 'Image de chat',
-    buttonLabel: 'Bouton de la card',
+    buttonLabel: 'Bouton de la card'
   },
-  {
-    description: 'Description de la card',
-    title: '100% ecolo',
-    imageSrc: 'http://placekitten.com/300/150',
-    imageAlt: 'Image de chat',
-    buttonLabel: 'Bouton de la card',
-  },
-  {
-    description: 'Description de la card',
-    title: 'Retours gratuits',
-    imageSrc: 'http://placekitten.com/300/150',
-    imageAlt: 'Image de chat',
-    buttonLabel: 'Bouton de la card',
-  },
-  {
-    description: 'Description de la card',
-    title: 'Retours gratuits',
-    imageSrc: 'http://placekitten.com/300/150',
-    imageAlt: 'Image de chat',
-    buttonLabel: 'Bouton de la card',
-  },
-  {
-    description: 'Description de la card',
-    title: 'Retours gratuits',
-    imageSrc: 'http://placekitten.com/300/150',
-    imageAlt: 'Image de chat',
-    buttonLabel: 'Bouton de la card',
-  },
+
   {
     id: '3',
     description: 'Description de la card',
     title: 'Retours gratuits',
     imageSrc: 'http://placekitten.com/300/150',
     imageAlt: 'Image de chat',
-    buttonLabel: 'Bouton de la card',
+    buttonLabel: 'Bouton de la card'
   },
-];
+  {
+    id: '2',
+    description: 'Description de la card',
+    title: 'Retours gratuits',
+    imageSrc: 'http://placekitten.com/300/150',
+    imageAlt: 'Image de chat',
+    buttonLabel: 'Bouton de la card'
+  }
+]
 
-const cards = reactive(response);
+const cards = reactive(response)
 </script>
 
 <template>
